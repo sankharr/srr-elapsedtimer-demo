d9543e9d59617a3aad003597284ceff17c32c6f8
Sankha Rathnayake <sankharathnayaka@gmail.com>
2020-11-08 23:51:56 +0530

    configured with the module + firebase added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ElapsedtimerService } from "srr-elapsedtimer";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,62 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jstTesting2';
+  timeNow: string;
+  currentHour: any;
+  currentMinute: any;
+  currentSeconds: any;
+
+  constructor(
+    private timer: ElapsedtimerService
+  ){}
+
+  getTime(){
+    this.timeNow = this.timer.getCurrentTime()
+  }
+
+  getHour(){
+    this.currentHour = this.timer.getCurrentHour()
+  }
+
+  getMinute(){
+    this.currentMinute = this.timer.getCurrentMinute()
+  }
+
+  getSeconds(){
+    this.currentSeconds = this.timer.getCurrentSecond()
+  }
+
+  buttonClick(){
+    this.timeNow = this.timer.getCurrentTime();
+    console.log("Time now - ",this.timeNow," ",this.timer.getCurrentHour()," ",this.timer.getCurrentMinute()," ",this.timer.getCurrentSecond())
+    // this.timer.resetTimer();
+  }
+
+  startNow(){
+    this.timer.startTimer();
+  }
+
+  pauseTimer(){
+    this.timer.pauseTimer();
+  }
+
+  resumeTimer(){
+    this.timer.resumeTimer();
+  }
+
+  delayStart(){
+    this.timer.delayStart(2);
+  }
+
+  setTimerExceedColourChange(){
+    this.timer.setTimerExceedColourChange("red",0,0,2);
+  }
+
+  resetTimer(){
+    this.timer.resetTimer();
+  }
+
+  setSettings(){
+    this.timer.setTimerSettings('consolas','blue','70');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ElapsedtimerModule } from "srr-elapsedtimer";
 
 @NgModule({
   declarations: [
@@ -10,9 +11,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ElapsedtimerModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
